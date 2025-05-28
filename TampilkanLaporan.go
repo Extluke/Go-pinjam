0339a20ca71144396ef2a4bd9638485d5f488718
aariffaqiih <aarifrjf@gmail.com>
2025-05-28 12:19:21 +0700

    coba push

---
+++ TampilkanLaporan.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func TampilkanLaporan(dataNasabah []Nasabah) {
+	fmt.Println("\n+-------------------------------+")
+	fmt.Println("|         Laporan Pinjaman      |")
+	fmt.Println("+-------------------------------+")
+	fmt.Println("misalkan aku bikin perubahan ini")
+}
---
+++ UrutkanPinjaman.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func UrutkanPinjaman(dataNasabah []Nasabah) {
+	fmt.Println("\n+-------------------------------+")
+	fmt.Println("|       Pilih Metode Urut       |")
+	fmt.Println("+-------------------------------+")
+	fmt.Println("| 1. Selection Sort             |")
+	fmt.Println("| 2. Insertion Sort             |")
+	fmt.Println("+-------------------------------+")
+}

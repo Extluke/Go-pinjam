82cf4be0cb896f85d3acee8cf63a7f03dc0a8723
aariffaqiih <aarifrjf@gmail.com>
2025-06-10 23:31:37 +0700

    feature update: 6 - add binary search

--- 6-Search.go
+++ 6-Search.go
@@ -8,30 +8,76 @@ func sequentialSearch(data []Nasabah, target string) int {
 			return i
 		}
 	}
-
 	fmt.Println("❌ Data tidak ditemukan")
-	return -1 
+	return -1
+}
+
+func binarySearch(data []Nasabah, target string) int {
+	left, right := 0, len(data)-1
+	for left <= right {
+		mid := (left + right) / 2
+		if data[mid].Nama == target {
+			return mid
+		} else if data[mid].Nama < target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+	fmt.Println("❌ Data tidak ditemukan")
+	return -1
+}
+
+func binarySelectionSort(data []Nasabah) {
+	n := len(data)
+	for i := 0; i < n-1; i++ {
+		indexTerkecil := i
+		for j := i + 1; j < n; j++ {
+			if data[j].Nama < data[indexTerkecil].Nama {
+				indexTerkecil = j
+			}
+		}
+		data[i], data[indexTerkecil] = data[indexTerkecil], data[i]
+	}
 }
 
 func CariPeminjam(dataNasabah []Nasabah) {
-	var target string
+	fmt.Println("\n╔══════════════════════════════════════╗")
+	fmt.Println("║       🔍 Pilih Metode Pencarian      ║")
+	fmt.Println("╠══════════════════════════════════════╣")
+	fmt.Println("║ 1. 🎰 Sequential Search              ║")
+	fmt.Println("║ 2. 🎲 Binary Search                  ║")
+	fmt.Println("╚══════════════════════════════════════╝")
 
-	fmt.Println("╔═══════════════════════════════════════")
-	fmt.Print("║ 🔍 Masukkan nama yang ingin dicari : ")
-	fmt.Scanln(&target)
-	fmt.Println("╚═══════════════════════════════════════")
-
-	index := sequentialSearch(dataNasabah, target)
-	if index == -1 {
-		return
+	var pilihan int
+	for {
+		fmt.Print("👉 Pilihan: ")
+		fmt.Scanln(&pilihan)
+		if pilihan == 1 || pilihan == 2 {
+			break
+		}
+		fmt.Println("⚠️  Input tidak valid, silakan pilih ulang.")
 	}
 
-	nasabah := dataNasabah[index]
-	fmt.Println("\n📄 Data Peminjam Ditemukan:")
-	fmt.Println("╔══════════════════════════════════════╗")
-	fmt.Printf("║ 👤 Nama             : %s\n", nasabah.Nama)
-	fmt.Printf("║ 💸 Jumlah Pinjaman  : Rp.%.2f\n", nasabah.JumlahPinjaman)
-	fmt.Printf("║ 🕒 Tenor            : %d bulan\n", nasabah.Tenor)
-	
-	fmt.Println("╚══════════════════════════════════════╝")
+	var nama string
+	fmt.Print("Masukkan Nama Peminjam: ")
+	fmt.Scanln(&nama)
+
+	var index int
+	if pilihan == 1 {
+		index = sequentialSearch(dataNasabah, nama)
+	} else {
+		binarySelectionSort(dataNasabah)
+		index = binarySearch(dataNasabah, nama)
+	}
+
+	if index != -1 {
+		nasabah := dataNasabah[index]
+		fmt.Println("\n📄 Data Peminjam Ditemukan:")
+		fmt.Println("╔══════════════════════════════════════╗")
+		fmt.Printf("║ 👤 Nama             : %s\n", nasabah.Nama)
+		fmt.Printf("║ 💸 Jumlah Pinjaman  : Rp.%.2f\n", nasabah.JumlahPinjaman)
+		fmt.Printf("║ 🕒 Tenor            : %d bulan\n", nasabah.Tenor)
+		fmt.Println("╚══════════════════════════════════════╝")
+	}
 }
